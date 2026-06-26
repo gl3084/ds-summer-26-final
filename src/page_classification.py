d9ea3bf8d8f48535ee871a113f830302cf88dc2f
gl3084 <gl3084@nyu.edu>
2026-06-26 21:07:19 +0000

    added greenhouse category

--- src/page_classification.py
+++ src/page_classification.py
@@ -1,4 +1,21 @@
 import streamlit as st
+import pandas as pd
+
 
 def render():
-    st.title("Classification page")
+    st.title("This is the classification model page")
+
+    df_withelectric = pd.read_excel("CO2.xlsx")
+    df = df_withelectric.dropna()
+
+    def greenhouse_category(score):
+        if score <= 4:
+            return "Low"
+        elif score <= 7:
+            return "Moderate"
+        else:
+            return "High"
+
+    df["Greenhouse Category"] = df["Greenhouse Gas Score"].apply(greenhouse_category)
+    st.dataframe(df.sample(30))
+    
