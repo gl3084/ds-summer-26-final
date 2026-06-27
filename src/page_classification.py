5f25eb12f2d845ceae31511d98792e68526a3a3e
gl3084 <gl3084@nyu.edu>
2026-06-27 11:51:20 +0000

    created gas and hybrid dataset

--- src/page_classification.py
+++ src/page_classification.py
@@ -4,12 +4,15 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
 from sklearn.preprocessing import LabelEncoder
+from sklearn.tree import DecisionTreeClassifier
 
 def render():
     st.title("This is the classification model page")
 
     df_withelectric = pd.read_excel("CO2.xlsx")
-    df = df_withelectric.dropna()
+    df = df_withelectric[df_withelectric["Fuel"] != "Electricity"]
+    df = df[df["Fuel"] != "Hydrogen"]
+    df = df[df["Fuel"] != "Electricity/Hydrogen"]
 
     def greenhouse_category(score):
         if score <= 4:
@@ -20,5 +23,41 @@ def render():
             return "High"
 
     df["Greenhouse Category"] = df["Greenhouse Gas Score"].apply(greenhouse_category)
-    st.dataframe(df.sample(30))
     
+    st.write ("This model predicts whether a vehicle has a Low, Moderate, or High greenhouse gas performance category. The original Greenhouse Gas Score ranges from 1 to 10, but we grouped the scores into three categories: Low = 1-4, Moderate = 5-7, and High = 8-10.")
+    
+    # Display the category distribution
+    st.subheader("Category Distribution")
+    category_counts = df["Greenhouse Category"].value_counts().reindex(["Low", "Moderate", "High"]).reset_index()
+    category_counts.columns = ["Greenhouse Category", "Count"]
+    st.dataframe(category_counts, use_container_width=True, hide_index=True)
+
+    # Keep gasoline MPG value for fuel flex/hybrid cars and convert to integers
+    df["City MPG"] = df["City MPG"].str.split("/").str[0]
+    df["Hwy MPG"] = df["Hwy MPG"].str.split("/").str[0]
+    df["Cmb MPG"] = df["Cmb MPG"].str.split("/").str[0]
+
+    df["City MPG"] = pd.to_numeric(df["City MPG"])
+    df["Hwy MPG"] = pd.to_numeric(df["Hwy MPG"])
+    df["Cmb MPG"] = pd.to_numeric(df["Cmb MPG"])
+
+    # Drop target/leakage/not useful predictive features for the model
+    drop_cols = ["Model", "Underhood ID", "Stnd", "Stnd Description", "Comb CO2", "Greenhouse Gas Score", "Greenhouse Category"]
+    model_df = df.drop(columns=drop_cols)
+    st.dataframe(model_df.head(5))
+    st.dataframe(model_df.dtypes)
+
+    # Encode categorical columns
+    categorical_cols = model_df.select_dtypes(include=["object"]).columns
+    for col in categorical_cols:
+        le = LabelEncoder()
+        model_df[col] = le.fit_transform(model_df[col])
+    X = model_df
+
+    target_encoder = LabelEncoder()
+    y = target_encoder.fit_transform(df["Greenhouse Category"])
+
+    
+
+
+
--- streamlit_app.py
+++ streamlit_app.py
@@ -89,9 +89,12 @@ with st.sidebar:
 
 # ── Set up dataset ────────────────────────────────────────────
 df = pd.read_excel("CO2.xlsx")
+
+st.title("THIS IS ALL TESTING SCROLL DOWN TO SEE YOUR PAGE")
 st.dataframe(df.head(5))
 
 st.write(df.shape)
+st.write(df["Fuel"].value_counts())
 
 st.markdown("##### Missing values")
 missing = df.isnull().sum()
@@ -108,8 +111,10 @@ if st.button("Show Original Dataset Stats"):
 
 
 # Removed electric cars which are all the ones with missing values
-st.markdown("##### NO ELECTRIC CAR DATASET")
-df_noelectric = df.dropna()
+st.markdown("##### NO ELECTRIC OR HYBRID CAR DATASET")
+df_noelectric = df[df["Fuel"] == "Gasoline"]
+st.dataframe(df_noelectric.sample(30))
+st.write(df_noelectric.dtypes)
 st.write(df_noelectric.columns)
 st.write(df_noelectric["Greenhouse Gas Score"].value_counts().sort_index())
 st.write(df_noelectric.shape)
@@ -126,6 +131,14 @@ else:
 st.markdown("##### 📈 Summary Statistics of only Gas Cars")
 if st.button("Show Updated Dataset Stats"):
     st.dataframe(df_noelectric.describe())
+
+st.markdown("##### GAS AND HYBRID CAR DATASET")
+df_gas_and_hybird = df[df["Fuel"] != "Electricity"]
+df_gas_and_hybird = df_gas_and_hybird[df_gas_and_hybird["Fuel"] != "Hydrogen"]
+df_gas_and_hybird = df_gas_and_hybird[df_gas_and_hybird["Fuel"] != "Electricity/Hydrogen"]
+st.write(df_gas_and_hybird["Fuel"].value_counts())
+
+st.divider()
 
 # ── Render selected page ────────────────────────────────────────────
 PAGES[selected].render()
