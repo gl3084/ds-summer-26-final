b7e1b95e90115c51d384999f76ca15702f3345e3
gl3084 <gl3084@nyu.edu>
2026-06-27 14:59:55 +0000

    completed model

--- src/page_classification.py
+++ src/page_classification.py
@@ -9,8 +9,23 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import cross_val_score
 
+
 def render():
-    st.title("This is the classification model page")
+    st.markdown("""
+    <style>
+    div.stButton > button {
+        background-color: #16a34a;
+        color: white;
+        border-radius: 8px;
+        font-weight: bold;
+        width: 100%;
+    }
+    
+    </style>
+    """, unsafe_allow_html=True)
+
+
+    st.title("🌳 Decision Tree Model")
 
     df_withelectric = pd.read_excel("CO2.xlsx")
     df = df_withelectric[df_withelectric["Fuel"] != "Electricity"]
@@ -29,11 +44,14 @@ def render():
     
     st.write ("This model predicts whether a vehicle has a Low, Moderate, or High greenhouse gas performance category. The original Greenhouse Gas Score ranges from 1 to 10, but we grouped the scores into three categories: Low = 1-4, Moderate = 5-7, and High = 8-10.")
     
+
     # Display the category distribution
     st.subheader("Category Distribution")
     category_counts = df["Greenhouse Category"].value_counts().reindex(["Low", "Moderate", "High"]).reset_index()
     category_counts.columns = ["Greenhouse Category", "Count"]
     st.dataframe(category_counts, use_container_width=True, hide_index=True)
+    st.divider()
+
 
     # Keep gasoline MPG value for fuel flex/hybrid cars and convert to integers
     df["City MPG"] = df["City MPG"].str.split("/").str[0]
@@ -43,6 +61,7 @@ def render():
     df["City MPG"] = pd.to_numeric(df["City MPG"])
     df["Hwy MPG"] = pd.to_numeric(df["Hwy MPG"])
     df["Cmb MPG"] = pd.to_numeric(df["Cmb MPG"])
+
 
     # Drop target/leakage/not useful predictive features for the model
     drop_cols = ["Model", 
@@ -56,6 +75,7 @@ def render():
                  "Air Pollution Score"]
     model_df = df.drop(columns=drop_cols)
 
+
     # Encode categorical columns
     categorical_cols = model_df.select_dtypes(include=["object"]).columns
     for col in categorical_cols:
@@ -66,43 +86,68 @@ def render():
     target_encoder = LabelEncoder()
     y = target_encoder.fit_transform(df["Greenhouse Category"])
 
+
+    # Let user control
+    st.subheader("Select Features to Train Model")
+
+    col1, col2, = st.columns([3,1])
+    with col1:
+        all_features = list(X.columns)
+        selected_features = st.multiselect("Select explanatory variables", options=all_features, default=all_features)
+    with col2:
+        test_size = st.slider("Test Size (%)", min_value=10, max_value=40, value=20)
+
     # Train model
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+    X_selected = X[selected_features]
+    X_train, X_test, y_train, y_test = train_test_split(X_selected, y, test_size=(test_size/100), random_state=42, stratify=y)
     
-    tree = DecisionTreeClassifier(max_depth=4, random_state=42)
-    tree.fit(X_train, y_train)
-    y_pred = tree.predict(X_test)
+    st.write(f"Training set: {len(X_train):,} samples &nbsp;&nbsp;|&nbsp;&nbsp;"
+             f"Test set: {len(X_test):,} samples")
+    train_button = st.button("🚀 Train Decision Tree Model", use_container_width=True)
 
-    st.subheader("Model Performace")
-    scores = cross_val_score(tree, X, y, cv=5)
-    st.write("Cross-validation scores: ", scores)
-    st.success(f"Average accuracy: {scores.mean()}")
+    if train_button:
+        if len(selected_features) == 0:
+            st.warning("⚠️ Please select at least one explanatory variable.")
 
-    labels = target_encoder.classes_
+        else:   
+            # Create Decision Tree    
+            tree = DecisionTreeClassifier(max_depth=4, random_state=42)
+            tree.fit(X_train, y_train)
+            y_pred = tree.predict(X_test)
 
-    # Classification report
-    st.subheader("Classification Report")
-    report = metrics.classification_report(y_test, y_pred, target_names=labels, output_dict=True)
-    report_df = pd.DataFrame(report).transpose()
-    st.dataframe(report_df, use_container_width=True)
+            st.subheader("🎯 Model Performace")
+            accuracy = metrics.accuracy_score(y_test, y_pred)
+            st.success(f"Accuracy: {accuracy:.2%}")
 
-    # Confusion matrix
-    st.subheader("Confusion Matrix")
-    cm = metrics.confusion_matrix(y_test, y_pred, labels=target_encoder.transform(["Low", "Moderate", "High"]))
-    fig, ax = plt.subplots(figsize = (6,5))
-    sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", xticklabels=["Low", "Moderate", "High"], yticklabels=["Low", "Moderate", "High"], ax=ax)
-    ax.set_xlabel("Predicted")
-    ax.set_ylabel("Actual")
-    ax.set_title("Confusion Matrix")
-    st.pyplot(fig)
+            labels = target_encoder.classes_
 
-    # Feature Importance
-    st.subheader("Top Feature Importances")
-    importance_df = pd.DataFrame({"Feature": X.columns, "Importance": tree.feature_importances_}).sort_values(by="Importance", ascending=False)
 
-    st.dataframe(importance_df, use_container_width=True)
-    st.bar_chart(importance_df.set_index("Feature")["Importance"])
+            # Classification report
+            st.subheader("📝 Classification Report")
+            report = metrics.classification_report(y_test, y_pred, target_names=labels, output_dict=True)
+            report_df = pd.DataFrame(report).transpose()
+            st.dataframe(report_df, use_container_width=True)
 
-    st.write(tree.get_depth())
-    st.write(tree.get_n_leaves())
-    st.write("Feautre used in model: ", list(X.columns))
+
+            # Confusion matrix
+            st.subheader("🤔 Confusion Matrix")
+            cm = metrics.confusion_matrix(y_test, y_pred, labels=target_encoder.transform(["Low", "Moderate", "High"]))
+            fig, ax = plt.subplots(figsize = (6,5))
+            sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", xticklabels=["Low", "Moderate", "High"], yticklabels=["Low", "Moderate", "High"], ax=ax)
+            ax.set_xlabel("Predicted")
+            ax.set_ylabel("Actual")
+            ax.set_title("Confusion Matrix")
+            st.pyplot(fig)
+
+
+            # Feature Importance
+            st.subheader("📌 Top Feature Importances")
+            importance_df = pd.DataFrame({"Feature": X_selected.columns, "Importance": tree.feature_importances_}).sort_values(by="Importance", ascending=False)
+
+            st.dataframe(importance_df, use_container_width=True)
+            fig2, ax2 = plt.subplots(figsize = (7,5))
+            ax2.barh(importance_df["Feature"], importance_df["Importance"])
+            ax2.set_xlabel("Importance")
+            ax2.set_ylabel("Feature")
+            ax2.set_title("Top Feature Importances")
+            st.pyplot(fig2)
