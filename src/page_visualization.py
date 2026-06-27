59b01af8d1bba8a775600b3676cca279bd0f6295
Matthew Hill <smatthewhill@gmail.com>
2026-06-27 19:40:32 -0400

    v2

--- src/page_visualization.py
+++ src/page_visualization.py
@@ -60,3 +60,27 @@ def render():
     fig.update_traces(textposition="inside", textinfo="percent+label")
 
     st.plotly_chart(fig, use_container_width=True)
+
+    
+        # ---- ROW 1: Manufacturer, Vehicle Class ----
+    st.title("Data Classifications")
+    row1_col1, row1_col2 = st.columns(2)
+
+    with row1_col1:
+        make_pie("Manufacturer", key_suffix="manufacturer")
+
+    with row1_col2:
+        make_pie("Veh Class", key_suffix="vehicle_class")
+
+    # ---- ROW 2: Fuel, Drive, Cylinders ----
+    st.subheader("Fuel, Drive & Cylinders")
+    row2_col1, row2_col2, row2_col3 = st.columns(3)
+
+    with row2_col1:
+        make_pie("Fuel", key_suffix="fuel")
+
+    with row2_col2:
+        make_pie("Drive", key_suffix="drive")
+
+    with row2_col3:
+        make_pie("Cylinders", key_suffix="cyl")
